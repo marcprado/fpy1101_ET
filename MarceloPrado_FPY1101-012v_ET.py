d4f46178a92a43600d5313edd25a243e24bb288a
marcprado <marc.prado@duocuc.cl>
2024-07-11 21:50:45 -0400

    Add files via upload

---
+++ MarceloPrado_FPY1101-012v_ET.py
@@ -0,0 +1,114 @@
+
+import csv
+import os
+import random
+
+sueldos = []
+
+random.randint (1,10)
+
+trabajadores = ["Juan Pérez”,”María García”,”Carlos López”,”Ana Martínez”,”Pedro Rodríguez”,”Laura Hernández”,”Miguel Sánchez”,”Isabel Gómez”,”Francisco Díaz”,”Elena Fernández"]
+def asignar_sueldos ():
+    print ("Sueldos de empleados")
+    nombre = input("Nombre empleado")
+    sueldos = int(input("Sueldo del empleado"))
+    calsificar_sueldo = int(input("Clasifique los sueldos"))
+    ver_estadi = int(input("Mostar estadisticas"))
+    respor_sueldo = int(input("Mostrar reporte de sueldos"))
+    sueldos = sueldos()
+    random.randint (1,10)
+    while sueldos == 0:
+        try:
+            import os
+            print ("1>Asignar sueldos aleatorios, 2>Clasificar sueldos, 3>Ver estadisticas, 4>Reporte de sueldos, 5>Salir del programa")
+            respuesta = int(input("Asignar sueldo"))
+            random.randint (300000, 2500000)
+            if respuesta == 1:
+                nombre += int(input("Ingrese el nombre"))
+                sueldos = 0
+            elif respuesta == 2:
+                calsificar_sueldo += int(input("clasifique los sueldos"))
+                sueldos = 0
+            elif respuesta == 3:
+                ver_estadi += int(input("Mostrar estadisticas"))
+                sueldos = 0
+            elif respuesta == 4:
+                respor_sueldo += int(input("Ingrese reporte de sueldo"))
+                sueldos = 0
+            elif respuesta == 5:
+                print ("Salir del programa")
+                respuesta = 6
+            break
+        except ValueError:
+            print("Ingresaste algo incorrecto")
+    def limpiar_pantalla():
+        limpiar_pantalla
+
+    
+trabajadores = ["Juan Pérez”,”María García”,”Carlos López”,”Ana Martínez”,”Pedro Rodríguez”,”Laura Hernández”,”Miguel Sánchez”,”Isabel Gómez”,”Francisco Díaz”,”Elena Fernández"]  
+sueldos = sueldos()
+def clasificar_sueldos():
+
+    random.randint (1,1000000)
+    print("Sueldos menores a $000.000 TOTAL: ")
+    print(f"Nombre empleado: " +trabajadores )
+    ['TOTAL: ']
+    print (f"Sueldo: ", +sueldos  )  
+    limpiar_pantalla
+    random.randint (1, 2000000)
+    print("Sueldos entre $000.000 y $2.000.000 ")
+    print(f"Nombre empleado: " +trabajadores )
+    print (f"Sueldo: ", +sueldos  )
+    ['TOTAL: '] 
+    limpiar_pantalla
+    random.randint (2000000, 10000000)
+    print ("Sueldos superiores a $2.000.000")
+    print(f"Nombre empleado: " +trabajadores )
+    print (f"Sueldo: ", +sueldos  )
+    ['TOTAL: ']
+    limpiar_pantalla
+    print (F"TOTAL SUELDOS:", +sueldos)
+
+def limpiar_pantalla():
+    limpiar_pantalla
+
+lista = []
+def ver_estadisticas():
+    if lista:
+        ultimo=pedido=max(int(pedido[N*
+pedido] for pedido in lista))
+    else : 
+        ultimo = pedido- numero-aleatorio
+    return ultimo-pedido+1
+
+limpiar_pantalla
+
+print (f"Sueldo mas alto" " ")
+print (f"Sueldo mas bajo" "  ")
+print (f"Promedio de sueldos")
+producto = 0   
+def media_geometrica(data):
+    producto = 1
+    for valor in data:
+        producto*=valor
+    return producto **(1/len(data))
+limpiar_pantalla
+
+def limpiar_pantalla():
+    limpiar_pantalla
+
+
+def reporte_de_sueldos ():
+    print (f"Nombre empleado, " " "), print (f"Sueldo Base, " " "), print(f"Descuento Salud", " "), print(f"Descuento AFP"), print(f"Sueldo Liquido")
+    print (f"Nombre empleado, " " "), print (f"Sueldo Base, " " "), print(f"Descuento Salud", " "), print(f"Descuento AFP"), print(f"Sueldo Liquido")
+def limpiar_pantalla():
+    limpiar_pantalla
+
+
+def salir_del_programa():
+    print ("Finalizando programa..")
+    print ("Desarrollado por Marcelo Prado")
+    print ("RUT 20.993.105-2")
+salir_del_programa
+limpiar_pantalla
+
